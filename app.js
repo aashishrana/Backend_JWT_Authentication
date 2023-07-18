5c607a4ed5a5b63b28982db749db55cd43247562
aashishrana <aashishrana71135@gmail.com>
2023-07-18 02:03:39 -0700

    facing error in making database connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const authRouter = require("./router/authRoute.js");
+const databaseconnect = require("./config/databaseConfig.js");
+
+databaseconnect();
 
 app.use(express.json());
 
