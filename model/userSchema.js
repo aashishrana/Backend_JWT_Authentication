653d1e004d52a8f4ba5976f3d8dd3fe10bc05885
aashishrana <aashishrana71135@gmail.com>
2023-07-21 04:09:52 -0700

    in model userSchema created successfullu

---
+++ model/userSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const userSchema = new Schema ({
+    name : {
+        type: String,
+        required : [true, 'user name is required'],
+        minLength : [5, 'Name must be at least 5 character'],
+        maxLength : [50, 'Name must be less than 50 character'],
+        trim : true
+    },
+
+    email : {
+        type : String,
+        required : [true, 'email is required'],
+        unique : [true, 'already registerd'],
+        lowercase : [true],
+    },
+
+    password : {
+        type : String,
+        select : false
+    },
+
+    forgotPasswordToken : {
+        type : String
+    },
+
+    forgotPasswordExpiryDate : {
+        type : Date
+    }
+
+});
+
+const userModel = mongoose.model("user", userSchema);
+module.exports = userModel;
