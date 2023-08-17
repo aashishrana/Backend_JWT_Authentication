a6607cc6a0dbcfb32f219178e3dd374668306f37
aashishrana <aashishrana71135@gmail.com>
2023-08-16 22:07:14 -0700

    database file

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -5,7 +5,7 @@ const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/my_dat
 const databaseconnect = () => {
     mongoose
         .connect(MONGODB_URL)
-        .then((conn) => console.log(`Connected to DB`))
+        .then((conn) => console.log(`Connected to DB: ${conn.connection.host}`))
         .catch((err) => console.log(err.message));
     
 }
