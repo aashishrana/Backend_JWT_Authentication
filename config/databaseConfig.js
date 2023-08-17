6c1e4107e35adc56d990b67594305c582276c95d
aashishrana <aashishrana71135@gmail.com>
2023-08-17 14:35:12 -0700

    MongoDB atlas Database connected successfully

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/my_database";
+const MONGODB_URL = process.env.MONGO_URL || "mongodb://localhost:27017/my_database";
 
 const databaseconnect = () => {
     mongoose
