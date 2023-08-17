ac7bea5b13938c8d66221ae75f6663365c989f4d
aashishrana <aashishrana71135@gmail.com>
2023-08-17 15:35:42 -0700

    signup controller completed

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,35 @@
 const userModel = require("../model/userSchema");
 
+const emailValidator = require("email-validator");
+
 const signup = async(req, res, next) => {
 
     const { name, email, password, confirmPassword} = req.body;
     console.log(name, email , password, confirmPassword);
+
+    if (!name || !email || !password || !confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "Every field is required"
+        })
+    }
+
+
+    const validEmail = emailValidator.validate(email);
+
+    if(!validEmail) {
+        return res.status(400).json({
+            success : false,
+            message : "Please provide a valid email id"
+        })
+    }
+
+    if(password !== confirmPassword) {
+        return res.status(400).json({
+            success : false,
+            message : "password and confirmPassword does not match"
+        })
+    }
 
     try {
 
