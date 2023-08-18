ff5bed259455a61f789ee188086d8bde6e95241d
aashishrana <aashishrana71135@gmail.com>
2023-08-18 13:52:04 -0700

    jwt middleware for verifying token

---
+++ middleware/jwtAuth.js
@@ -0,0 +1,9 @@
+const JWT = require("jsonwebtoken");
+
+const jwtAuth = (req , res , next) => {
+
+
+    next();
+}
+
+module.exports = jwtAuth;
--- router/authRoute.js
+++ router/authRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const { signup , signin , getUser} = require("../controller/authController");
+const jwtAuth = require("../middleware/jwtAuth");
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', signup);
 authRouter.post("/signin" , signin)
-authRouter.get("/getuser", getUser)
+authRouter.get("/getuser", jwtAuth , getUser)
 // authRouter.post('/router', controller);
 
 module.exports = authRouter; 
